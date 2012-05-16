80cec541cf5ef60014714f81a9dc5805f4ddd095
myfreeweb <floatboth@me.com>
2012-05-16 20:40:57 +0400

    add devserve function

--- rapidmachine/__init__.py
+++ rapidmachine/__init__.py
@@ -3,8 +3,11 @@
 version_info = (0, 1, 0)
 __version__ = ".".join(map(str, version_info))
 
+def devserve(app, port=5000):
+    from werkzeug.serving import run_simple
+    run_simple('127.0.0.1', port, app, use_debugger=True, use_reloader=True)
+
 try:
-    from rapidmachine.decisions import process
     from rapidmachine.resource import Resource
 except ImportError:
     import traceback
