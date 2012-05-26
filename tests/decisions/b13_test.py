0684f42e7512684e10576bd5018239e6cb88d594
myfreeweb <floatboth@me.com>
2012-05-26 17:18:38 +0400

    remove ping test

--- tests/decisions/b13_test.py
+++ tests/decisions/b13_test.py
@@ -1,12 +1,12 @@
 import t
 
 class b13(t.Test):
-    
+
     class TestResource(t.Resource):
-        
+
         available = True
         pong = True
-        
+
         def ping(self, req, rsp):
             print self.pong
             return self.pong
@@ -17,7 +17,7 @@ class b13(t.Test):
 
         def to_html(self, req, rsp):
             return "nom nom"
-    
+
     def test_ok(self):
         self.TestResource.available = True
         self.TestResource.pong = True
@@ -25,13 +25,6 @@ class b13(t.Test):
         t.eq(self.rsp.status_code, 200)
         t.eq(self.rsp.response, ['nom nom'])
 
-    def test_no_ping(self):
-        self.TestResource.available = True
-        self.TestResource.pong = False
-        self.go()
-        t.eq(self.rsp.status_code, 503)
-        t.eq(self.rsp.response, [])
-    
     def test_no_service(self):
         self.TestResource.available = False
         self.TestResource.pong = True
